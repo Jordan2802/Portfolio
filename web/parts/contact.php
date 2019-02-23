9c076f1815852140f388abb0083d5fe79a3bb70c
Jordan2802 <sha08dow@hotmail.fr>
2019-02-23 17:53:48 +0100

    logo
    logo + fond

--- web/parts/contact.php
+++ web/parts/contact.php
@@ -1,3 +1,33 @@
-<div class="fond1"></div>
+<div class="fond1">
+<form>
+<div class="row">
+    <div class="col">
+    <label for="exampleFormControlInput1">Prénom</label>
+      <input type="text" class="form-control" placeholder="">
+    </div>
+    <div class="col">
+    <label for="exampleFormControlInput1">Nom</label>
+      <input type="text" class="form-control" placeholder="">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Email</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Message</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+  </div>
+  <div class="col-auto">
+      <button type="submit" class="btn btn-primary mb-2">Envoyer</button>
+    </div>
+</form>
+</div>
 
-<div class="fond2"></div>
+<div class="fond2">
+    <div class="mescontacts">
+        <h2> <span>Contact :</span> OUDOT Jordan</h2>
+        <p> <span>téléphone :</span> 06/15/86/26/64</p>
+        <p> <span>Email :</span> jordan.oudot@jordanoweb.fr</p>
+    </div>
+</div>
